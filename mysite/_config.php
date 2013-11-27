f5685147dfc08e0989059bcbd8cc15c33c40055e
Ruud Arentsen <ruud@thewebmen.com>
2013-11-27 09:42:43 +0100

    removed legacy extension and environment type

--- mysite/_config.php
+++ mysite/_config.php
@@ -14,11 +14,10 @@ $databaseConfig = array(
 );
 
 if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
-	Director::set_environment_type("dev");
+	Config::inst()->update('Director', 'environment_type', 'dev');
 }
 
 // Set the site locale
 i18n::set_locale('nl_NL');
 
 SS_Cache::set_cache_lifetime('any', 86400);
-Object::add_extension('SiteConfig', 'CustomSiteConfig');
